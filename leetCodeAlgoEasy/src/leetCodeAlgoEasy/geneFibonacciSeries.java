fdfdab60bf3103c654fe9b37bee6b5f5975003d9
sirijami <sirishaepari@192.168.0.100>
2017-09-12 16:27:23 -0700

    Generate fibonacci series
    O(n) complexity. It can be solved using recursion too.

---
+++ leetCodeAlgoEasy/src/leetCodeAlgoEasy/geneFibonacciSeries.java
@@ -0,0 +1,33 @@
+package leetCodeAlgoEasy;
+
+public class geneFibonacciSeries {
+
+	public static void main(String[] args) {
+		int firstNum = 10;
+		fibonacciSeries(firstNum);
+	}
+
+	private static void printArray(int[] arr) {
+		for(int i = 0; i < arr.length ; i++){
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();		
+	}
+
+	private static int[] fibonacciSeries(int firstNum) {
+		int[] result  = new int[10];
+		int prevFirstNum = 0;
+		int temp = 0;		
+		result[0] = prevFirstNum;
+		result[1] = firstNum;
+		for(int i = 2 ; i < 10; ++i ){
+			temp =  firstNum + prevFirstNum;
+			result[i] = temp;
+			prevFirstNum = firstNum;
+			firstNum = temp;			
+		}
+		printArray(result);
+		return result;		
+	}
+
+}
