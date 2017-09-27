a210572d151db00b05f8945d76f222fee1c1c2cf
sirijami <sirishaepari@192.168.0.100>
2017-09-27 13:04:15 -0700

    few minor changes

--- AlgoAssignments/src/sumClosestToZero.java
+++ AlgoAssignments/src/sumClosestToZero.java
@@ -1,19 +1,18 @@
 import java.util.Arrays;
-
-
 public class sumClosestToZero {
 
 	public static void main(String[] args) {
-		int[] arr = {2,1,92,478,3,56};
-		
+		int[] arr = {2,1,92,478,3,56};	
 		sumPair(arr);
-
-
 	}
 
 	private static void sumPair(int[] arr) {
 		int sum, min = 99999;
 		int left = 1, right = arr.length-1;
+		if(arr.length < 2){
+			System.out.println("Not a valid input");
+			return;
+		}
 		Arrays.sort(arr);
 		int i = 0;
 		int r = arr.length -1;
